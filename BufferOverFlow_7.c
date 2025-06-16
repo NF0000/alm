3e513ac0ba696ef4811179802287853c9f268ec2
NF0000 <nfnf31444@gmil.com>
2025-06-16 14:02:19 +0900

    変更

--- BufferOverFlow_7.c
+++ BufferOverFlow_7.c
@@ -3,11 +3,19 @@
 int main ( void ){
 int check = 0;
 char pass[8];
+printf("%d-%d-%d-%d\n"
+,&pass[0],&pass[1],&pass[7],&check);
+printf("%c-%c-%c-%d\n"
+,pass[0] ,pass[1] ,pass[7] ,check);
 printf("Input Password\n");
 scanf("%s",pass);
+printf("%c-%c-%c-%d\n"
+,pass[0] ,pass[1] ,pass[7] ,check);
 if ( strcmp(pass,"Pass") == 0 ){
 check = 1;
 }
+printf("%c-%c-%c-%d\n"
+,pass[0] ,pass[1] ,pass[7] ,check);
 if ( check ) printf("OK\n");
 else printf("NG\n");
 return 0;
