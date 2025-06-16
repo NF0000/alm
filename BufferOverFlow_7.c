7ecc7aa70d2893bbfa1804e0afe70e189615aecc
NF0000 <nfnf31444@gmil.com>
2025-06-16 14:35:24 +0900

    更新

--- BufferOverFlow_7.c
+++ BufferOverFlow_7.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 int main ( void ){
-int check = 0;
-char pass[8];
-printf("%d-%d-%d-%d\n"
-,&pass[0],&pass[1],&pass[7],&check);
-printf("%c-%c-%c-%d\n"
-,pass[0] ,pass[1] ,pass[7] ,check);
-printf("Input Password\n");
-scanf("%s",pass);
-printf("%c-%c-%c-%d\n"
-,pass[0] ,pass[1] ,pass[7] ,check);
-if ( strcmp(pass,"Pass") == 0 ){
-check = 1;
-}
-printf("%c-%c-%c-%d\n"
-,pass[0] ,pass[1] ,pass[7] ,check);
-if ( check ) printf("OK\n");
-else printf("NG\n");
-return 0;
+    int check = 0;
+    char pass[8];
+
+    printf("%d-%d-%d-%d\n",&pass[0],&pass[1],&pass[7],&check);
+    printf("%c-%c-%c-%d\n",pass[0] ,pass[1] ,pass[7] ,check);
+
+    printf("Input Password\n");
+    fgets(pass,8,stdin);
+
+    printf("%c-%c-%c-%d\n",pass[0] ,pass[1] ,pass[7] ,check);
+
+    if ( strcmp(pass,"Pass") == 0 ){
+    check = 1;
+    }
+
+    printf("%c-%c-%c-%d\n",pass[0] ,pass[1] ,pass[7] ,check);
+    if ( check ) printf("OK\n");
+    else printf("NG\n");
+
+    return 0;
 }
